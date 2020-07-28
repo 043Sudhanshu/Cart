bea0e524527e97836a6087ceafa23ca2791875e3
sudhanshu chauhan <043sudhanshu@gmail.com>
2020-07-28 12:43:27 +0530

    increase and decrease functions are passed from Cartitem component to Cart.To increase and decrease the quantity

--- src/Cart.js
+++ src/Cart.js
@@ -4,16 +4,75 @@ import Cartitem from './Cartitem';
 
 class Cart extends React.Component{
 
+    constructor(){
+        super();
+        this.state={
+            products:[
+                        {
+                            price:25000,
+                            qty:4,
+                            title:'PS4',
+                            id:1
+                        },
+
+                    {
+                        price:55000,
+                        qty:1,
+                        title:'AC',
+                        id:2
+                    },
+
+                        {
+                            price:60000,
+                            qty:1,
+                            title:'Snooker table',
+                            id:3
+                        }
+            ]
+        }
+    }
+     
+    increaseq=(product)=>{
+     const {products}=this.state;
+     const index=products.indexOf(product);
+     
+     products[index].qty+=1;
+     this.setState({
+         products:products         // the right product is the product which we have taken above by destructuring..we are updating it
+     });
+
+    } 
+    decreaseq=(product)=>{
+        const {products}=this.state;
+        const index=products.indexOf(product);
+        if( products[index].qty>1){
+        products[index].qty-=1;
+        }
+        this.setState({
+            products:products         // the right product is the product which we have taken above by destructuring..we are updating it
+        });
+   
+       }
+
+
   render(){
 
     return (
+
         <div className="Cart"> 
-            <Cartitem/>
-            <Cartitem/>
-            <Cartitem/>
-            <Cartitem/>
-            <Cartitem/>
-            
+        
+         {
+          this.state.products.map( (product) =>{
+        
+            return <Cartitem 
+            product={product} 
+            increasequantity={this.increaseq}
+            decreasequantity={this.decreaseq}
+            key={this.id}
+            /> 
+           } )
+        }  
+        
         </div>
     );
 
--- src/Cartitem.js
+++ src/Cartitem.js
@@ -2,31 +2,10 @@ import React from 'react';
 
 class Cartitem extends React.Component{
    
-    constructor(){
-        super();
-       this.state={
-           price:900,
-           qty:1,
-           title:'Mobile phone'
-       }  
-    }
-    increasequantity= () => {
-     this.setState((prevstate)=>{
-      return { qty:prevstate.qty+1}
-     });  
-    }
-    decreasequantity= () =>{
-        const {qty} =this.state;
-        if(qty===0){
-            return;
-        }
-        this.setState((prevstate)=>{
-            return { qty:prevstate.qty - 1 }
-        });
-    }
-   
     render(){
-        const {price,title,qty} =this.state;      //object destructuring..directy use the property of this.state
+       
+        const {price,title,qty} =this.props.product;      //object destructuring..directy use the property of this.state
+        const {product,increasequantity,decreasequantity}=this.props;
         return(
             <div className="cart-item">
                 <div  className="left-block">
@@ -38,9 +17,19 @@ class Cartitem extends React.Component{
                         <div style={{color:'#777'}} >{qty}</div>
                       <div className="cart-item-actions">
                                 {/* Buttons -comment */}
-                <img onClick={this.increasequantity} alt="increase" className="action-icons" src="https://image.flaticon.com/icons/svg/992/992651.svg" />
-                <img onClick={this.decreasequantity} alt="decrease" className="action-icons" src="https://image.flaticon.com/icons/svg/1665/1665612.svg" />
-                <img alt="delete" className="action-icons" src="https://image.flaticon.com/icons/svg/1214/1214428.svg" />
+                            <img 
+                            onClick={()=>increasequantity(product)}   // this function shuld be called onclicking because it increasing the quantity of product
+                            alt="increase" className="action-icons" 
+                            src="https://image.flaticon.com/icons/svg/992/992651.svg" 
+                            />
+                            <img onClick={()=>decreasequantity(product)} 
+                            alt="decrease" className="action-icons" 
+                            src="https://image.flaticon.com/icons/svg/1665/1665612.svg" 
+                            />
+                            <img 
+                            alt="delete" className="action-icons" 
+                            src="https://image.flaticon.com/icons/svg/1214/1214428.svg" 
+                            />
                      </div>
                  </div>
             </div>
